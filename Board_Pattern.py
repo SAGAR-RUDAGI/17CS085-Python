d518cc316c1adf9956fcf7421a536c99bf1e7f1f
SAGAR RUDAGI <sagarrudagi@gmail.com>
2019-11-26 10:11:10 +0530

    Add files via upload

---
+++ Board_Pattern.py
@@ -0,0 +1,11 @@
+for i in range(3):
+	for j in range(3):
+		print(" ",end="")
+		print('-'*3,end="")
+		print(" ",end="")
+		print()
+		print("| "*4)
+for j in range(3):
+	print(" ",end="")
+	print('-'*3,end="")
+	print(" ",end="")
